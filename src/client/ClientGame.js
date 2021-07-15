8caf70978851ab1fd0e32d8c02ab6af1315f25c1
KRASAVA <vladimirova.a-a@yandex.ru>
2021-07-15 21:31:10 +0400

    Add movable boy

--- src/client/ClientGame.js
+++ src/client/ClientGame.js
@@ -2,22 +2,29 @@ import ClientEngine from './ClientEngine';
 import sprites from '../configs/sprites';
 import levelCfg from '../configs/world.json';
 import ClientWorld from './ClientWorld';
+import gameObjects from '../configs/gameObjects.json';
 
 class ClientGame {
   constructor(cfg) {
-    Object.assign(this, { cfg });
+    Object.assign(this, { cfg, gameObjects, player: null });
 
     this.engine = this.createEngine();
     this.map = this.createWorld();
     this.initEngine();
   }
 
+  setPlayer(player) {
+    this.player = player;
+  }
+
   initEngine() {
     this.engine.loadSprites(sprites).then(() => {
+      this.map.init();
       this.engine.on('render', (_, time) => {
-        this.map.init();
+        this.map.render(time);
       });
       this.engine.start();
+      this.initKeys();
     });
   }
 
@@ -27,6 +34,31 @@ class ClientGame {
 
   createEngine() {
     return new ClientEngine(document.getElementById(this.cfg.tagId));
+  }
+
+  initKeys() {
+    this.engine.input.onKey({
+      ArrowLeft: (keyDown) => {
+        if (keyDown) {
+          this.player.moveByCellCoord(-1, 0, (cell) => cell.findObjectsByType('grass').length);
+        }
+      },
+      ArrowRight: (keyDown) => {
+        if (keyDown) {
+          this.player.moveByCellCoord(1, 0, (cell) => cell.findObjectsByType('grass').length);
+        }
+      },
+      ArrowUp: (keyDown) => {
+        if (keyDown) {
+          this.player.moveByCellCoord(0, -1, (cell) => cell.findObjectsByType('grass').length);
+        }
+      },
+      ArrowDown: (keyDown) => {
+        if (keyDown) {
+          this.player.moveByCellCoord(0, 1, (cell) => cell.findObjectsByType('grass').length);
+        }
+      },
+    });
   }
 
   static init(cfg) {
